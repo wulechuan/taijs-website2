7c332083b1341278eb977be549133f6bfbd64a78
wulechuan <wulechuan@live.com>
2016-06-27 17:20:32 +0800

    fixed qna dd expanding issue

--- app-dev/scripts/base/base.js
+++ app-dev/scripts/base/base.js
@@ -1,7 +1,12 @@
 (function () {
 	window.console = window.console || { log: function () {} };
 	$('dl.initially-collapsed dt').on('click', function (event) {
-		// $(this).find('+ dd').toggleClass('expanded');
-		$(this).find('+ dd').slideToggle();
+		var $dd = $(this).find('+ dd');
+		if ($dd.hasClass('expanded')) {
+			$dd.slideUp();
+		} else {
+			$dd.slideDown();
+		}
+		$dd.toggleClass('expanded');
 	});
 })();
