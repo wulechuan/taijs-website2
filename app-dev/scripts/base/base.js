1f429cd435a1ef6a9ba42c64acb45364538b02fe
wulechuan <wulechuan@live.com>
2016-06-27 17:14:18 +0800

    qna list ok but ie8 mark style incorrect

--- app-dev/scripts/base/base.js
+++ app-dev/scripts/base/base.js
@@ -1,3 +1,7 @@
 (function () {
 	window.console = window.console || { log: function () {} };
+	$('dl.initially-collapsed dt').on('click', function (event) {
+		// $(this).find('+ dd').toggleClass('expanded');
+		$(this).find('+ dd').slideToggle();
+	});
 })();
