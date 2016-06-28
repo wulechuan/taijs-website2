f74133854aa8fe59e3715fd143b1a145f1f96314
wulechuan <wulechuan@live.com>
2016-06-28 11:23:25 +0800

    ie8 sliding works without animation

--- app-dev/scripts/base/base.js
+++ app-dev/scripts/base/base.js
@@ -1,128 +1,162 @@
 (function () {
 	window.console = window.console || { log: function () {} };
+
+
 	$('dl.initially-collapsed').each(function (index, dl) {
-		var isIE8 = navigator.userAgent.match(/msie 8/i);
-		var isIE9 = navigator.userAgent.match(/msie 9/i);
+		var isIE8 = !!navigator.userAgent.match(/msie 8/i);
+		var isIE9 = !!navigator.userAgent.match(/msie 9/i);
+
 
 		$allDTs = $(this).find('> dt');
 		$allDDs = $(this).find('> dd');
 
-		// if (isIE8) {
-		// 	$allDDs.each(function (index, dd) {
-		// 		dd.style.transitionProperty = 'none';
-		// 	});
-		// }
+		$allDTs.each(function (index, dt) {
+			var dd = $(dt).find('+ dd')[0];
+			var ddContent = $(dd).find('> .content')[0];
 
-		if (isIE9) {
-			$allDDs.each(function (index, dd) {
+			dd.elements = { dt: dt, content: ddContent };
+			dt.elements = { dd: dd };
+
+
+			// dt.setAttribute('number-in-list', index);
+			// dd.setAttribute('number-in-list', index);
+
+			if (isIE8) {
+				// dd.style.visibility = 'hidden';
+			}
+
+			if (isIE9) {
 				dd.style.display = 'none';
 
 				// height values and transitions would effect jQuery sliding
 				dd.style.height = 'auto';
-				dd.style.transitionProperty = 'none';
-			});
-		}
+				// dd.style.transitionProperty = 'none';
+			}
+		});
 
 
 
 
 		$allDTs.on('click', function (event) {
-			var $thisDT = $(this);
-			var thisDD = $thisDT.find('+ dd')[0];
+			var thisDD = this.elements.dd;
 
-			var newHeightOfDLForIE8 = 0;
-			var ddHeight;
+			if (isIE8) {
+				var forceUpdatingContainer = dl.parentNode
+					// || $(this).parents('.page-content')[0]
+					// || $(this).parents('.page')[0]
+					// || document.body
+				;
+			}
+
+			var dlNewHeight = 0;
+			var accumHeight;
+
 			for (var i = 0; i < $allDDs.length; i++) {
 				var dd = $allDDs[i];
 				if (dd === thisDD) {
-					ddHeight = _processOneDD(dd, 'toggle');
+					accumHeight = _processOnePair(dd, 'toggle');
 				} else {
-					ddHeight = _processOneDD(dd, 'collapse');
+					accumHeight = _processOnePair(dd, 'collapse');
 				}
 
-				if (isIE8 && ddHeight) newHeightOfDLForIE8 += ddHeight;
+				if (isIE8 && accumHeight) dlNewHeight += accumHeight;
 			}
 
 			if (isIE8) {
-				for (var i = 0; i < $allDTs.length; i++) {
-					newHeightOfDLForIE8 += $($allDTs[i]).outerHeight();
-				}
-				dl.style.height = newHeightOfDLForIE8+'px';
+				dl.style.height = dlNewHeight + 'px';
+				forceUpdatingContainer.visibility = 'hidden';
+				setTimeout(function () {
+					forceUpdatingContainer.visibility = '';
+				}, 0);
 			}
 
 
-			function _processOneDD(dd, action) {
+			function _processOnePair(dd, action) {
+				var ddBottomBorderWidth = 1;
 				var $dd = $(dd);
-				var wasCollapsed = !$dd.hasClass('expanded');
-				var needAction = (!wasCollapsed && action==='collapse') || (action==='toggle');
-				if (!needAction) return 0;
+				var $dt = $(dd.elements.dt);
+				var content = dd.elements.content;
 
+				var dtHeight = 0;
+				var ddNewHeight = 0;
+				var ddContentCurrentHeight;
 
 				if (isIE8) {
+					dtHeight = $dt.outerHeight();
+				}
+
+				var wasCollapsed = !$dd.hasClass('expanded');
+				var needAction = (!wasCollapsed && action==='collapse') || (action==='toggle');
+				if (!needAction) {
+					if (isIE8) {
+						return dtHeight;
+					}
+
+					return 0;
+				}
+
+
+				if (isIE8) { // update className BEFORE animation
 					if (wasCollapsed) {
-						var content = $dd.find('> .content')[0];
-						var ddExpandedHeight = $(content).outerHeight();
+						ddContentCurrentHeight = $(content).outerHeight();
+						ddNewHeight = ddContentCurrentHeight + ddBottomBorderWidth;
 
 						$dd.addClass('expanded');
-						$thisDT.addClass('expanded');
+						$dt.addClass('expanded');
 
-						dd.style.height = ddExpandedHeight+'px';
+						dd.style.height = ddNewHeight + 'px';
+
+						return dtHeight+ddNewHeight;
+					} else {
+						$dd.removeClass('expanded');
+						$dt.removeClass('expanded');
+
+						dd.style.height = '0px';
+
+						return dtHeight;
+					}
+				} else if (isIE9) { // update className BEFORE animation
+					if (wasCollapsed) {
+						$dd.addClass('expanded');
+						$dt.addClass('expanded');
 
 						setTimeout(function () {
 							$dd.slideDown();
 						}, 0);
-
-						return ddExpandedHeight;
 					} else {
 						$dd.removeClass('expanded');
-						$thisDT.removeClass('expanded');
-
-						dd.style.height = '';
+						$dt.removeClass('expanded');
 
 						setTimeout(function () {
 							$dd.slideUp();
 						}, 0);
-
-						return 0;
 					}
-				}
-
-				if (isIE9) {
+				} else { // update className AFTER transition
 					if (wasCollapsed) {
-						$dd.slideDown();
-					} else {
-						$dd.slideUp();
-					}
-				}
-
-				if (!isIE8 && !isIE9) {
-					if (wasCollapsed) {
-						var content = $dd.find('> .content')[0];
-						if (content.knownExpandedHeight > 20) {
+						if (content.knownHeight > 20) {
 							setTimeout(function () {
-								var newExpandedHeight = $(content).outerHeight();
-								if (newExpandedHeight !== content.knownExpandedHeight) {
-									content.knownExpandedHeight = newExpandedHeight;
-									dd.style.height = content.knownExpandedHeight+'px';
+								var ddContentCurrentHeight = $(content).outerHeight();
+								if (ddContentCurrentHeight !== content.knownHeight) {
+									content.knownHeight = ddContentCurrentHeight;
+									ddNewHeight = ddContentCurrentHeight + ddBottomBorderWidth;
+									dd.style.height = content.ddNewHeight+'px';
 								}
-							}, 200);
+							}, 100);
 						} else {
-							content.knownExpandedHeight = $(content).outerHeight();
+							content.knownHeight = $(content).outerHeight();
 						}
 
-						dd.style.height = content.knownExpandedHeight+'px';
+						ddNewHeight = content.knownHeight + ddBottomBorderWidth;
+						dd.style.height = ddNewHeight+'px';
+
+						$dd.addClass('expanded');
+						$dt.addClass('expanded');
 					} else {
 						dd.style.height = '';
+
+						$dd.removeClass('expanded');
+						$dt.removeClass('expanded');
 					}
-				}
-
-
-				if (wasCollapsed) {
-					$dd.addClass('expanded');
-					$thisDT.addClass('expanded');
-				} else {
-					$dd.removeClass('expanded');
-					$thisDT.removeClass('expanded');
 				}
 
 
