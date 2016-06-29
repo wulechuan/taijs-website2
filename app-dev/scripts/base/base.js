94bb5118890fa5a007ffb4da36dfd42ad881a264
wulechuan <wulechuan@live.com>
2016-06-29 19:31:36 +0800

    aria for panels; logics for all panel-tab-sets

--- app-dev/scripts/base/base.js
+++ app-dev/scripts/base/base.js
@@ -2,13 +2,70 @@
 	window.console = window.console || { log: function () {} };
 
 
+
+	$('.tab-panel-set').each(function () {
+		var $allTabs = $(this).find('.tab-list > li');
+
+		$allTabs.each(function (index, tab) {
+			var panel = $('#'+tab.getAttribute('aria-controls'))[0];
+
+			if (!panel) return false;
+
+			panel.elements = { tab: tab };
+			tab.elements = { panel: panel };
+
+			if (index === 0) {
+				_processOnePair(tab, true);
+			}
+		});
+
+		$allTabs.on('click', function () {
+			_showPanelViaTab(this);
+		});
+
+		function _showPanelViaTab(theTab) {
+			for (var i = 0; i < $allTabs.length; i++) {
+				var tab = $allTabs[i];
+				_processOnePair(tab, tab === theTab);
+			}
+		}
+
+		function _processOnePair(tab, isToShownMyPanel) {
+			var panel = tab.elements.panel;
+
+			if (!panel) return false;
+
+			// if (isIE8) {
+			// 	var forceUpdatingContainer = dl.parentNode
+			// 		// || $(this).parents('.page-content')[0]
+			// 		// || $(this).parents('.page')[0]
+			// 		// || document.body
+			// 	;
+			// }
+
+			if (isToShownMyPanel) {
+				panel.setAttribute('aria-hidden', false);
+				$(tab).addClass('current');
+				$(panel).addClass('current');
+			} else {
+				panel.setAttribute('aria-hidden', true);
+				$(tab).removeClass('current');
+				$(panel).removeClass('current');
+			}
+
+			panel.style.display = isToShownMyPanel ? 'block' : 'none';
+		}
+	});
+
+
+
 	$('dl.initially-collapsed').each(function (index, dl) {
 		var isIE8 = !!navigator.userAgent.match(/msie 8/i);
 		var isIE9 = !!navigator.userAgent.match(/msie 9/i);
 
 
-		$allDTs = $(this).find('> dt');
-		$allDDs = $(this).find('> dd');
+		var $allDTs = $(this).find('> dt');
+		var $allDDs = $(this).find('> dd');
 
 		$allDTs.each(function (index, dt) {
 			var dd = $(dt).find('+ dd')[0];
@@ -17,13 +74,8 @@
 			dd.elements = { dt: dt, content: ddContent };
 			dt.elements = { dd: dd };
 
-			// dt.setAttribute('number-in-list', index);
-			// dd.setAttribute('number-in-list', index);
 			dd.setAttribute('aria-expanded', false);
-
-			if (isIE8) {
-				// dd.style.visibility = 'hidden';
-			}
+			dd.setAttribute('aria-hidden', true);
 
 			if (isIE9) {
 				dd.style.display = 'none';
@@ -37,11 +89,12 @@
 
 
 
-		$allDTs.on('click', function (event) {
+		$allDTs.on('click', function () {
 			var thisDD = this.elements.dd;
 
+			var forceUpdatingContainer = null;
 			if (isIE8) {
-				var forceUpdatingContainer = dl.parentNode
+				forceUpdatingContainer = dl.parentNode
 					// || $(this).parents('.page-content')[0]
 					// || $(this).parents('.page')[0]
 					// || document.body
@@ -102,16 +155,18 @@
 						ddContentCurrentHeight = $(content).outerHeight();
 						ddNewHeight = ddContentCurrentHeight + ddBottomBorderWidth;
 
-						$dd.addClass('expanded')
+						$dd.addClass('expanded');
 						dd.setAttribute('aria-expanded', true);
+						dd.setAttribute('aria-hidden',   false);
 						$dt.addClass('expanded');
 
 						dd.style.height = ddNewHeight + 'px';
 
 						return dtHeight+ddNewHeight;
 					} else {
-						$dd.removeClass('expanded')
+						$dd.removeClass('expanded');
 						dd.setAttribute('aria-expanded', false);
+						dd.setAttribute('aria-hidden',   true);
 						$dt.removeClass('expanded');
 
 						dd.style.height = '0px';
@@ -120,13 +175,15 @@
 					}
 				} else if (isIE9) { // update className BEFORE animation
 					if (wasCollapsed) {
-						$dd.addClass('expanded')
+						$dd.addClass('expanded');
 						dd.setAttribute('aria-expanded', true);
+						dd.setAttribute('aria-hidden',   false);
 						$dt.addClass('expanded');
 						$dd.slideDown();
 					} else {
-						$dd.removeClass('expanded')
+						$dd.removeClass('expanded');
 						dd.setAttribute('aria-expanded', false);
+						dd.setAttribute('aria-hidden',   true);
 						$dt.removeClass('expanded');
 						$dd.slideUp();
 					}
@@ -148,14 +205,16 @@
 						ddNewHeight = content.knownHeight + ddBottomBorderWidth;
 						dd.style.height = ddNewHeight+'px';
 
-						$dd.addClass('expanded')
+						$dd.addClass('expanded');
 						dd.setAttribute('aria-expanded', true);
+						dd.setAttribute('aria-hidden',   false);
 						$dt.addClass('expanded');
 					} else {
 						dd.style.height = '';
 
-						$dd.removeClass('expanded')
+						$dd.removeClass('expanded');
 						dd.setAttribute('aria-expanded', false);
+						dd.setAttribute('aria-hidden',   true);
 						$dt.removeClass('expanded');
 					}
 				}
@@ -193,13 +252,13 @@
             	level1: psn1,
             	level2: psn2
             }
-        }
+        };
     }
 
     function setPageSidebarNavCurrentItem(conf) {
     	conf = conf || {};
 		conf.level1IdPrefix = 'menu-psn-1-';
-		var level1CurrentItem = setMenuCurrentItemForLevel(1, 2, $('#page-sidebar-nav'), conf);
+		setMenuCurrentItemForLevel(1, 2, $('#page-sidebar-nav'), conf);
     }
 
     function setMenuCurrentItemForLevel(level, depth, parentDom, conf) {
